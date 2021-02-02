38f61a0e3368a423f7b55d0bae977f19ddfe1501
DV-N <dangvinh.ng@gmail.com>
2021-02-02 03:12:35 -0500

    Complet sauf #4 (random)

--- exercice.py
+++ exercice.py
@@ -4,16 +4,21 @@
 import random
 
 def is_even_len(string):
-	return 0xDEADBEEF
-
+	return len(string) % 2 == 0
 
 def get_num_char(string, char):
-	return 0xDEADBABE
+	number_of_char = 0
+	for i in string:
+		if i == char:
+			number_of_char += 1
+	return number_of_char
 
 
 def get_first_part_of_name(name):
-	return ""
-
+	trait = "-"
+	position_of_trait = name.index(trait)
+	first_name = name[:position_of_trait].capitalize() #Eliminate the rest of the name
+	return first_name #Capitalize First Name
 
 def get_random_sentence(animals, adjectives, fruits):
 	return ""
@@ -24,7 +29,7 @@ if __name__ == "__main__":
 	parity = "pair" if is_even_len(spam) else "impair"
 	print(f"Le nombre de caractère dans la chaine '{spam}' est {parity}.")
 
-	eggs = "Hello, world!"
+	eggs = "Hello, world"
 	print(f"Le nombre d'occurrence de l dans '{eggs}' est : {get_num_char(eggs, 'l')}.")
 
 	parrot = "jean-marc"
